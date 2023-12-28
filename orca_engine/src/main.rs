ac69d569fe2a1409fbd7287fcc7113dc96fbadd6
Brian Welman <brianallisterwelman@gmail.com>
2023-12-28 16:44:47 +0200

    ORCa-Engine source added

---
+++ orca_engine/src/main.rs
@@ -0,0 +1,80 @@
+mod utils;
+
+use log::debug;
+use utils::{GaussianProcess, Kernels};
+use rand_distr::{Normal, Distribution};
+use hdf5::File;
+
+struct Axis {
+    mu: f64,
+    sigma: f64,
+}
+
+fn measurement_operator(input: f64) -> f64 {
+    // Example: Damped harmonic oscillator
+    0.5 * ((2.0 * std::f64::consts::PI * input).cos() * (-input / 8.0).exp())
+}
+
+fn simulate_data(input_data: &Vec<f64>, meas_axis: Axis) -> (Vec<f64>, Vec<f64>) {
+    let mut rng = rand::thread_rng();
+    let meas_dist = Normal::new(meas_axis.mu, meas_axis.sigma).unwrap();
+
+    let output_data: Vec<f64> = input_data
+                                .iter()
+                                .map(|&x| measurement_operator(x))
+                                .collect();
+                            
+    let noisy_data: Vec<f64> = output_data
+                               .iter()
+                               .map(|&x| x + meas_dist.sample(&mut rng))
+                               .collect();
+
+    (output_data, noisy_data)
+}
+
+fn main() -> hdf5::Result<()> {
+    // Initialise `log4rs` from config file
+    log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
+
+    // Gaussian Process Parameters
+    const N_TIME: usize = 1000; 
+    let kernel = Kernels::SqrExp.kernel();
+    let gp = GaussianProcess::new(1.0, 1.0, 50.0, kernel, N_TIME);
+    debug!("Gaussian Process (GP) instantiated.");
+
+    // Generate data using Gaussian Process
+    let input_data: Vec<f64> = gp.generate().iter().cloned().collect();
+    debug!("Input data created from GP.");
+
+    println!("{:?}", &input_data);
+    // Measurable Signal Axis
+    let meas_axis = Axis { mu: 0.0, sigma: 0.5 };
+
+    // Simulate data using the measurement operator and noise
+    let (output_data, noisy_data) = simulate_data(&input_data, meas_axis);
+    debug!("Output data created from input data.");
+
+    // Create a new HDF5 file
+    let file = File::create("data.h5").unwrap();
+    let group = file.create_group("data").unwrap();
+
+    // Create datasets
+    group.new_dataset::<f64>()
+        .create("input_data", input_data.len())
+        .unwrap()
+        .write(&input_data)
+        .unwrap();
+    group.new_dataset::<f64>()
+        .create("output_data", output_data.len())
+        .unwrap()
+        .write(&output_data)
+        .unwrap();
+    group.new_dataset::<f64>()
+        .create("noisy_data", noisy_data.len())
+        .unwrap()
+        .write(&noisy_data)
+        .unwrap();
+
+    Ok(())
+}   
+
---
+++ orca_engine/src/utils.rs
@@ -0,0 +1,97 @@
+extern crate rand_distr;
+extern crate nalgebra as na;
+
+use rand::Rng;
+use rand_distr::StandardNormal;
+use na::{DMatrix, DVector, SVD};
+use log::{info, debug, error};
+
+type KernelPointer = Box<dyn Fn(f64, f64, f64, f64) -> f64>;
+
+pub fn squared_exponential_kernel(x: f64, y: f64, std: f64, scale: f64) -> f64 {
+    std.powi(2) * (-0.5 * ((x - y) / scale).powi(2)).exp()
+}
+
+pub enum Kernels {
+    SqrExp,
+}
+
+impl Kernels {
+    pub fn kernel(&self) -> KernelPointer {
+        match self {
+            Kernels::SqrExp => Box::new(squared_exponential_kernel),
+        }
+    }
+}
+
+pub struct GaussianProcess {
+    mean: f64,
+    std: f64,
+    scale: f64,
+    kernel: KernelPointer,
+    length: usize,
+}
+
+impl GaussianProcess {
+    pub fn new(mean: f64, std: f64, scale: f64,
+        kernel: KernelPointer, length: usize) -> Self {
+        Self {
+            mean,
+            std,
+            scale,
+            kernel,
+            length,
+        }
+    }
+
+    pub fn generate(&self) -> DVector<f64> {
+        // Calculate covariance matrix
+        let covariance_matrix = self.calculate_covariance_matrix();
+
+        // Perform SVD
+        let svd = SVD::new(covariance_matrix, true, true);
+        let u = svd.u.expect("Failed to compute U matrix");
+        let singular_values = svd.singular_values;
+        let v_t = svd.v_t.expect("Failed to compute V^T matrix");
+
+        // Convert singular values to a diagonal matrix
+        let sigma_matrix = DMatrix::from_diagonal(&singular_values.map(|x| x.sqrt()));
+
+        // Generate standard normal samples
+        let mut rng = rand::thread_rng();
+        let z: DVector<f64> = DVector::from_iterator(self.length, (0..self.length).map(|_| rng.sample(StandardNormal)));
+        debug!("Axis samples drawn.");
+
+        // Transform using SVD components
+        let samples = u * sigma_matrix * v_t.transpose() * z;
+        debug!("Derived GP from SVD and samples.");
+
+        // Add the mean
+        info!("Generated Gaussian Process data.");
+        samples.map(|v| v + self.mean)
+    }
+
+    fn calculate_covariance_matrix(&self) -> DMatrix<f64> {
+        let mut matrix = DMatrix::zeros(self.length, self.length);
+        for i in 0..self.length {
+            let norm_i = i as f64 / self.length as f64;
+            for j in 0..self.length {
+                let norm_j = j as f64 / self.length as f64;
+                
+                // Use the kernel function to compute the covariance
+                matrix[(i, j)] = squared_exponential_kernel(norm_i, norm_j, self.std, self.scale);
+            }
+        }
+        matrix
+    }
+}
+
+fn condition_number(matrix: &DMatrix<f64>) -> f64 {
+    let svd = matrix.clone().svd(true, true);
+    let singular_values = svd.singular_values;
+
+    let max_singular = singular_values.max();
+    let min_singular = singular_values.min();
+
+    max_singular / min_singular
+}
