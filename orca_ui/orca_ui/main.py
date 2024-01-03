de0533174722307c65a06286a7309f1359c75349
Brian Welman <brianallisterwelman@gmail.com>
2024-01-03 14:41:29 +0200

    Simple plotting to test orca-engine

--- orca_ui/orca_ui/main.py
+++ orca_ui/orca_ui/main.py
@@ -1,5 +1,18 @@
 import h5py
-from matplotlib import pyplot
+import numpy as np
+from matplotlib import pyplot as plt
 
-f = h5py.File("data.h5", "r")
-list(f.keys())
+f = h5py.File("../../orca_engine/data.h5", "r")
+y1 = np.array(f["data"]["output_data"])
+y2 = np.array(f["data"]["noisy_data"])
+x = np.arange(y1.size)
+
+f.close()
+
+plt.plot(x, y1, "k-", label="True")
+plt.plot(x, y2, "r:", label="Noisy")
+plt.xlabel("Time")
+plt.ylabel("Value")
+plt.legend()
+plt.title("Simulated Output Data")
+plt.show()
