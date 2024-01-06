6f8fe4ae3776186d6942a39e17f4b46f0de44de9
Brian Welman <brianallisterwelman@gmail.com>
2024-01-06 13:27:36 +0200

    Plotting experiment results

--- orca_ui/orca_ui/main.py
+++ orca_ui/orca_ui/main.py
@@ -3,16 +3,22 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 f = h5py.File("../../orca_engine/data.h5", "r")
-y1 = np.array(f["data"]["output_data"])
-y2 = np.array(f["data"]["noisy_data"])
-x = np.arange(y1.size)
+true_data = np.array(f["data"]["output_data"])
+noisy_data = np.array(f["data"]["noisy_data"])
+pre_data = np.array(f["data"]["mp"])
+fil_data = np.array(f["data"]["mf"])
+smo_data = np.array(f["data"]["ms"])
+
+x = np.arange(true_data.size)
 
 f.close()
 
-plt.plot(x, y1, "k-", label="True")
-plt.plot(x, y2, "r:", label="Noisy")
+plt.plot(x, true_data, "k-", lw=2.0, label="True")
+plt.plot(x, pre_data, "r:", label="Predictor")
+plt.plot(x, fil_data, "g--", label="Filter")
+plt.plot(x, smo_data, "b-", label="Smoother")
 plt.xlabel("Time")
 plt.ylabel("Value")
 plt.legend()
-plt.title("Simulated Output Data")
+plt.title("Simulated Kalman Filter and Smoother")
 plt.show()
