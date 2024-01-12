279dc28bd9c8329e70072b885434dc6d07399f4c
Brian Welman <brianallisterwelman@gmail.com>
2024-01-12 11:23:33 +0200

    Better public float types from core

--- orca_engine/src/core.rs
+++ orca_engine/src/core.rs
@@ -1,6 +1,7 @@
 // core.rs
 
 // Reconcile data type names
-pub use num_complex::{Complex32, Complex64};
+pub use num_traits::{Float};
+pub use num_complex::{Complex, Complex32, Complex64};
 pub type Float32 = f32;
 pub type Float64 = f64;
